1c39354f709625a4b99fcf1e5ebe0c9f6e927a2b
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-18 13:06:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,4 +8,5 @@ html = open("https://flatironschool.com/")
   # that we can easily play around with.
 doc = Nokogiri::HTML(html)
 
+# call .css on doc and give it the argument of our CSS selector:
 doc.css(".headline-26OIBN")
