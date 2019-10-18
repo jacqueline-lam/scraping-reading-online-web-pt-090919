48eea15dbf2688cf796c794b600381fbf413fec0
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-18 13:05:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+# take the string of HTML returned by open-uri's open method 
+  # and convert it into a NodeSet (bunch of nested "nodes") 
+  # that we can easily play around with.
 Nokogiri::HTML(html)
