312c0bcf73f7d19cfb8a4eac89446e98c3567431
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-18 13:15:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -45,4 +45,5 @@ end
 p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
 # => "div"
 
-#.attributes will return ids, names, and classes,
+#.attributes will return ids, names, and classes
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].attributes
